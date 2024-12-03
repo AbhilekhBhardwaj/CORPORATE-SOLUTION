450fea459b77dad195cb6edc4f0374a3a203dd97
Abhilekh Bhardwaj <bhardwaj.abhilekh@gmail.com>
2024-12-03 13:19:46 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  // Set the base path to relative for proper resolution of assets and imports
+  base: './',
   plugins: [react()],
-  base: '/',
+  resolve: {
+    alias: {
+      // Optional: Use '@' as a shorthand for the 'src' directory
+      '@': '/src',
+    },
+  },
   build: {
-    outDir: 'dist',
-    sourcemap: true
-  }
-})
+    rollupOptions: {
+      external: ['/src/main.jsx'],
+      input: 'index.html',
+    },
+  },
+});
